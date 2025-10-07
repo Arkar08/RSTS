8ec73579beeff017b09074279c128407e3631ba5
uiux@okdollar <uiux@okdollar.com>
2025-10-07 15:56:08 +0530

    base url change

--- Frontend/src/config/ApiConfig.ts
+++ Frontend/src/config/ApiConfig.ts
@@ -3,7 +3,9 @@ import axios from 'axios'
 const Axios = axios.create()
 
 // Axios.defaults.baseURL = "https://rsts-vintage-shop-backend.onrender.com/api/v1/"
-Axios.defaults.baseURL = "http://localhost:8080/api/v1/"
+// Axios.defaults.baseURL = "http://localhost:8080/api/v1/"
+
+Axios.defaults.baseURL = "http://209.38.90.90/api/v1/"
 
 
 Axios.interceptors.request.use((config)=>{
